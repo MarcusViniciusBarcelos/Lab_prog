8ea9c7deaba6751ed9d057ebb25a6951e563a516
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2023-08-24 19:06:06 -0300

    exercicios aula 3

---
+++ exercicios/exercicios/program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continuar = true;
+
+            while (continuar)
+            {
+                MenuExercicios.MostrarMenu();
+                Console.Write("Escolha uma opção: ");
+                int opcao = Convert.ToInt32(Console.ReadLine());
+
+                if (opcao == 0)
+                {
+                    continuar = false;
+                    Console.WriteLine("Saindo...");
+                }
+                else
+                {
+                    MenuExercicios.ExecutarExercicio(opcao);
+                }
+            }
+        }
+    }
+}
